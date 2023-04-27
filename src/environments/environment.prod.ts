3f0658158a3ae7d0f2367823969eb2a15d1490e4
matiasdantemar <matiasdantemar@hotmail.com>
2023-04-27 08:15:11 -0300

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
-
-    production: true,
+  firebase: {
+    projectId: 'matias-portfolio',
+    appId: '1:267428264730:web:a67e28b74d547b4fe34aa3',
+    storageBucket: 'matias-portfolio.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDxmN90viZgb2ouU222T19kQHCb6BoldFk',
+    authDomain: 'matias-portfolio.firebaseapp.com',
+    messagingSenderId: '267428264730',
+  },
+    production: false,
     URL: 'http://produccion'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
-
-    production: false,
+  firebase: {
+    projectId: 'matias-portfolio',
+    appId: '1:267428264730:web:a67e28b74d547b4fe34aa3',
+    storageBucket: 'matias-portfolio.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDxmN90viZgb2ouU222T19kQHCb6BoldFk',
+    authDomain: 'matias-portfolio.firebaseapp.com',
+    messagingSenderId: '267428264730',
+  },
+    production: true,
     URL: 'http://localhost:8080/',
 };
